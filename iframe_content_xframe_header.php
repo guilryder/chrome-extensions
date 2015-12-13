d276a4730cf0f32b8c5d5fc33d833a9d1375633a
Guillaume Ryder <guillaume@ryder.fr>
2015-12-12 18:40:04 -0800

    xframe_ignore - adds test files.

---
+++ iframe_content_xframe_header.php
@@ -0,0 +1,9 @@
+<?php
+header('x-frame-options: deny', false /* replace */);
+header('X-FRAME-OPTIONS: deny', false /* replace */);
+header('X-Frame-Options: deny', false /* replace */);
+?>
+<html>
+<body>
+  iframe content - iframe_content_xframe_header.php
+</body>
