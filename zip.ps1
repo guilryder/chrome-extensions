bf2c55667be039cb9aad9014e68b337bbfa3ff4c
Guillaume Ryder <guillaume@ryder.fr>
2021-03-13 16:44:52 -0800

    Fixes a local path in the release script.

--- zip.ps1
+++ zip.ps1
@@ -6,7 +6,7 @@ Push-Location (Split-Path (Resolve-Path $MyInvocation.MyCommand.Path))
 
 foreach ($extension in $Extensions) {
   $extension = Split-Path -Leaf $extension
-  $extension_zip = "C:\Users\Guillaume\Google Drive\Perso\Chrome extensions\${extension}.zip"
+  $extension_zip = "C:\Users\Guillaume\Google Drive\Hobby\Chrome extensions\${extension}.zip"
   echo "$extension ..."
   rm $extension_zip
   zip.exe -jo9 $extension_zip "$extension/src/*"
